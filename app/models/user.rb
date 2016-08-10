6197dbca0ddfcf99e1e0732eeef7ac6db53cf62f
Chanwoo Kim <chanwoohill@gmail.com>
2016-08-10 21:14:14 +0000

    initial commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   has_many :teams
   has_many :lists
   
-  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
   validates :password, presence: true
   
 end 
