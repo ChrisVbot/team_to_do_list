2180a4b5e04acb47a374a338f40e2a427ece0acc
ChrisVbot <chris.veillette@gmail.com>
2016-08-11 02:40:04 +0000

    basic functionality

--- app/actions.rb
+++ app/actions.rb
@@ -1,9 +1,31 @@
 # Homepage (Root path)
+helpers do
+  def current_user
+    User.first
+  end
+end
+
 get '/' do
   erb :index
 end
 
-get '/user' do 
-  @user = User.first
+get '/user/?' do 
+  @user = current_user
+  @team = Team.new
   erb :'user/index'
+end
+
+post '/create_team' do
+  @team = Team.new(
+    name: params[:name]
+    )
+  # binding.pry
+  if @team.save
+    current_user.pairings.create(team: @team)
+    #flash - thanks for creating a team
+    redirect '/user'
+  else
+    erb :'user/index'# 
+  end
+
 end
