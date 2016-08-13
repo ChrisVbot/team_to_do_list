b76c253576af8ac96dfc12d6c5ccf9d98a14797c
Chanwoo Kim <chanwoohill@gmail.com>
2016-08-13 20:41:10 +0000

    add radial progress tracker

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -9,6 +9,20 @@ $(".complete_checkbox").click(function() {
 
 });
 
+$('.avatar-container').on('click',function(){
+  
+  if ($(this).find('.info').length <= 0) {
+    $(this).append('<div class="info"><div class="info-inner"></div></div>');
+  }
+  
+  var $info = $(this).find('.info'),
+      $inner= $(this).find('.info-inner'),
+      $p    = $(this).attr("class").match(/p-\w+/).toString().substring(2);
+  
+  $inner.text($p+'%');
+  $info.toggleClass('js-active');
+});
+
 // $( '.checkboxes' ).on( 'click', 'input:checkbox', function () {
 //     $( this ).next('label').toggleClass( 'highlight', this.checked );
 // });
